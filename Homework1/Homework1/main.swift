b32806477c7c80ed80b0bb4c8ad34707b95caea6
Сакен Аскар <sakenaskar79@MacBook-Air-Saken.local>
2026-09-08 14:00:00 +0500

    Completed Homework 1

---
+++ Homework1/Homework1/main.swift
@@ -0,0 +1,34 @@
+//Personal Information
+let firstName = "Saken"
+let lastName = "Askar"
+let BirthYear = 2005
+let CurrentYear = 2026
+let age = CurrentYear - BirthYear
+let isStudent = true
+let height = 1.78
+
+//Hobbies
+let hobby = "Gym"
+let numberofhobbies = 3
+let favouriteNumber = 7
+let isHobbyCreative = false
+
+//Add Info
+let university = "kbtu"
+let favouriteTeam = "Barcelona"
+
+//Lifestory
+let lifeStory = """
+My name is \(firstName) \(lastName). I am \(age) years old, born in \(BirthYear).
+I am a student at \(university) (student status: \(isStudent)).
+My height is \(height) m.
+My main hobby is \(hobby) (is it creative? \(isHobbyCreative)).
+I have \(numberofhobbies) hobbies in total, and my favorite number is \(favouriteNumber).
+My favorite football team is \(favouriteTeam).
+"""
+
+print (lifeStory)
+
+
+
+
